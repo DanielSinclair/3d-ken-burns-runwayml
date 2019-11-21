4e09dcbcc212266191850b8abd25daeb9d86e727
Daniel Sinclair <d@niel.nyc>
2019-11-20 19:08:04 -0500

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -29,14 +29,18 @@ import runway
 
 @runway.setup(options={})
 def setup(opts):
-    torch.set_grad_enabled(False)
-    torch.backends.cudnn.enabled = True
-    exec(open('./3d-ken-burns/common.py', 'r').read())
-    exec(open('./3d-ken-burns/models/disparity-estimation.py', 'r').read())
-    exec(open('./3d-ken-burns/models/disparity-adjustment.py', 'r').read())
-    exec(open('./3d-ken-burns/models/disparity-refinement.py', 'r').read())
-    exec(open('./3d-ken-burns/models/pointcloud-inpainting.py', 'r').read())
-    return process_load
+    try:
+        torch.set_grad_enabled(False)
+        torch.backends.cudnn.enabled = True
+        exec(open('./3d-ken-burns/common.py', 'r').read())
+        exec(open('./3d-ken-burns/models/disparity-estimation.py', 'r').read())
+        exec(open('./3d-ken-burns/models/disparity-adjustment.py', 'r').read())
+        exec(open('./3d-ken-burns/models/disparity-refinement.py', 'r').read())
+        exec(open('./3d-ken-burns/models/pointcloud-inpainting.py', 'r').read())
+        return process_load
+    except RunwayError as e:
+        print(e.code, e.message)
+        print(e.to_response())
 
 processingInput = {'image': runway.image(description="photograph")}
 processingOutput = {'video': runway.array(
@@ -46,32 +50,36 @@ processingOutput = {'video': runway.array(
 
 @runway.command('process', inputs = processingInput, outputs = processingOutput)
 def process(model, inputs):
-    numpyImage = numpy.image(inputs['image'])
-    intWidth = numpyImage.shape[1]
-    intHeight = numpyImage.shape[0]
-    dblRatio = float(intWidth) / float(intHeight)
-    intWidth = min(int(1024 * dblRatio), 1024)
-    intHeight = min(int(1024 / dblRatio), 1024)
-    numpyImage = cv2.resize(src=numpyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
-    model(numpyImage, {})
-    objectFrom = {
-      'dblCenterU': intWidth / 2.0,
-      'dblCenterV': intHeight / 2.0,
-      'intCropWidth': int(math.floor(0.97 * intWidth)),
-      'intCropHeight': int(math.floor(0.97 * intHeight))
-    }
-    objectTo = process_autozoom({
-      'dblShift': 100.0,
-      'dblZoom': 1.25,
-      'objectFrom': objectFrom
-    })
-    numpyResult = process_kenburns({
-      'dblSteps': numpy.linspace(0.0, 1.0, 75).tolist(),
-      'objectFrom': objectFrom,
-      'objectTo': objectTo,
-      'boolInpaint': True
-    })
-    return {'video': numpyResult}
+    try:
+        numpyImage = numpy.image(inputs['image'])
+        intWidth = numpyImage.shape[1]
+        intHeight = numpyImage.shape[0]
+        dblRatio = float(intWidth) / float(intHeight)
+        intWidth = min(int(1024 * dblRatio), 1024)
+        intHeight = min(int(1024 / dblRatio), 1024)
+        numpyImage = cv2.resize(src=numpyImage, dsize=(intWidth, intHeight), fx=0.0, fy=0.0, interpolation=cv2.INTER_AREA)
+        model(numpyImage, {})
+        objectFrom = {
+          'dblCenterU': intWidth / 2.0,
+          'dblCenterV': intHeight / 2.0,
+          'intCropWidth': int(math.floor(0.97 * intWidth)),
+          'intCropHeight': int(math.floor(0.97 * intHeight))
+        }
+        objectTo = process_autozoom({
+          'dblShift': 100.0,
+          'dblZoom': 1.25,
+          'objectFrom': objectFrom
+        })
+        numpyResult = process_kenburns({
+          'dblSteps': numpy.linspace(0.0, 1.0, 75).tolist(),
+          'objectFrom': objectFrom,
+          'objectTo': objectTo,
+          'boolInpaint': True
+        })
+        return {'video': numpyResult}
+    except RunwayError as e:
+        print(e.code, e.message)
+        print(e.to_response())
 
 if __name__ == '__main__':
     try:
